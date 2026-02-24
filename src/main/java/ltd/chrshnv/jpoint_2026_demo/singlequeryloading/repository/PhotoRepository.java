658ecaeee42e0fcc5c1ed7cb10cb03ff8a59a8db
Artemiy Degtyarev <chereshnevvv4real@gmail.com>
2026-02-24 10:37:28 +0500

    Remove photo example.

--- src/main/java/ltd/chrshnv/jpoint_2026_demo/singlequeryloading/repository/PhotoRepository.java
+++
@@ -1,7 +0,0 @@
-package ltd.chrshnv.jpoint_2026_demo.singlequeryloading.repository;
-
-import ltd.chrshnv.jpoint_2026_demo.singlequeryloading.entity.Photo;
-import org.springframework.data.repository.ListCrudRepository;
-
-public interface PhotoRepository extends ListCrudRepository<Photo, Long> {
-}
